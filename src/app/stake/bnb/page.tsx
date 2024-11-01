c7cea3fc75d22a38f44d8ebf39a15aca2f6b3f54
SunoukMun <52026496+munsunouk@users.noreply.github.com>
2024-11-01 18:43:03 +0900

    remove pages

--- src/app/stake/bnb/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import ListaStakingPage from "@/app/components/ListaStakingPage";
-
-const StakingAssetPage = ({ params }: { params: { asset: string } }) => {
-  return <ListaStakingPage />;
-};
-
-export default StakingAssetPage;
--- src/app/stake/eth/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import LidoStakingPage from "@/app/components/LidoStakingPage";
-
-const StakingAssetPage = ({ params }: { params: { asset: string } }) => {
-  return <LidoStakingPage />;
-};
-
-export default StakingAssetPage;
